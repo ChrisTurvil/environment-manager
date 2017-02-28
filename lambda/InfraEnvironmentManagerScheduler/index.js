01facabe27fa2a8662acd04168334858fbcb982e
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-02-28 09:18:04 +0000

    PD-80 Lambda config from environment variables InfraEnvironmentManagerScheduler

--- lambda/InfraEnvironmentManagerScheduler/index.js
+++ lambda/InfraEnvironmentManagerScheduler/index.js
@@ -1,24 +1,51 @@
 ﻿/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict';
 
-let config = require('./config.json');
+let process = require('process');
 let schedulerFactory = require('./scheduler.js');
 
 exports.handler = (event, context, callback) => {
+  const AWS_REGION = process.env.AWS_REGION;
+  const EM_HOST = process.env.EM_HOST;
+  const EM_USERNAME = process.env.EM_USERNAME;
+  const EM_PASSWORD = process.env.EM_PASSWORD;
+  const IGNORE_ASG_INSTANCES = process.env.IGNORE_ASG_INSTANCES || false;
+  const LIMIT_TO_ENVIRONMENT = process.env.LIMIT_TO_ENVIRONMENT;
+  const LIST_SKIPPED_INSTANCES = process.env.LIST_SKIPPED_INSTANCES || false;
+  const WHAT_IF = process.env.WHAT_IF || false;
 
+  let invocationRegion = context.invokedFunctionArn.split(':')[3];
   let account = context.invokedFunctionArn.split(':')[4];
+
+  let config = {
+    aws: {
+      region: AWS_REGION || invocationRegion
+    },
+    em: {
+      credentials: {
+        password: EM_PASSWORD,
+        username: EM_USERNAME
+      },
+      host: EM_HOST
+    },
+    ignoreASGInstances: IGNORE_ASG_INSTANCES,
+    limitToEnvironment: LIMIT_TO_ENVIRONMENT,
+    listSkippedInstances: LIST_SKIPPED_INSTANCES,
+    whatIf: WHAT_IF
+  }
+
   let scheduler = schedulerFactory.create(account, config);
 
   try {
 
     scheduler.doScheduling()
-    .then(result => {
-      if (result.success) callback(null, logSuccess(result));
-      else callback(logError('Scheduling Failure', result));
-    })
-    .catch(err => {
-      callback(logError('Unhandled Exception', err));
-    });
+      .then(result => {
+        if (result.success) callback(null, logSuccess(result));
+        else callback(logError('Scheduling Failure', result));
+      })
+      .catch(err => {
+        callback(logError('Unhandled Exception', err));
+      });
 
   } catch (err) {
     callback(logError('Unhandled Exception', err));
--- lambda/InfraEnvironmentManagerScheduler/local/index.js
+++ lambda/InfraEnvironmentManagerScheduler/local/index.js
@@ -1,13 +1,36 @@
 /* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict'
 
-let config = require('./config.json');
-let schedulerFactory = require('../scheduler.js');
+let process = require('process');
+let sut = require('../index');
 
-let account = 'enter-em-account-name';
-let scheduler = schedulerFactory.create(account, config);
+const environment = {
+  AWS_REGION: 'eu-west-1',
+  EM_HOST: 'http://localhost:8080/',
+  EM_USERNAME: 'my username',
+  EM_PASSWORD: 'my password',
+  IGNORE_ASG_INSTANCES: 'false',
+  LIMIT_TO_ENVIRONMENT: 'xxx',
+  LIST_SKIPPED_INSTANCES: 'true',
+  WHAT_IF: 'true'
+};
 
-scheduler
-  .doScheduling()
-  .then(result => { console.log(JSON.stringify(result, null, 2)); })
-  .catch(err => { console.log(JSON.stringify(err, null, 2)); });
+function setup() {
+  Object.keys(environment)
+    .forEach(variable => process.env[variable] = environment[variable]);
+}
+
+let event = {};
+
+let context = {
+  invokedFunctionArn: 'arn:aws:lambda:eu-west-1:<my-account-number>:function:<my-function-name>'
+}
+
+setup();
+sut.handler(event, context, (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.stringify(data, null, 2));
+  }
+});
