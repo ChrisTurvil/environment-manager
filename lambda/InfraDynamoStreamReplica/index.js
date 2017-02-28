9345c281b3d5d0a3a95437b0c0722138cf10e0ee
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-02-28 09:17:16 +0000

    PD-80 Lambda config from environment variables InfraDynamoStreamReplica

--- lambda/InfraDynamoStreamReplica/index.js
+++ lambda/InfraDynamoStreamReplica/index.js
@@ -2,90 +2,85 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const tableRegex = /arn:aws:dynamodb:[\w-]+:\d+:table\/([^\/]+)\/stream\/[\w-:\.]+/;
-const DESTINATION_ACCOUNTS = // Assign an array of all your Environment Manager slave accounts here.
+const process = require('process');
 
-/* This function returns a promise of the configuration required
- * to run this function. The current implementation just returns
- * a constant but a DynamoDB or S3 lookup would be easy. */
-function getConfiguration(context) {
-  return Promise.resolve({
-    DestinationAccounts: DESTINATION_ACCOUNTS
-  });
-}
+const tableRegex = /arn:aws:dynamodb:[\w-]+:\d+:table\/([^\/]+)\/stream\/[\w-:\.]+/;
 
 function getTableNameFromARN(arn) {
   var tableName = tableRegex.exec(arn)[1];
   return tableName;
 }
 
-function getIAMRoleInTargetAccount(accountNumber) {
-  var arn = "arn:aws:iam::" + accountNumber + ":role/roleInfraDynamoStreamReplicaWriter"
-  return arn;
-}
-
 function createReplicateRequest(record) {
-    let request = {
-        TableName: getTableNameFromARN(record.eventSourceARN)
+  let request = {
+    TableName: getTableNameFromARN(record.eventSourceARN)
+  };
+  if (record.eventName === 'REMOVE') {
+    request.Key = record.dynamodb.Keys;
+    return {
+      operation: 'deleteItem',
+      params: request
     };
-    if (record.eventName === 'REMOVE') {
-        request.Key = record.dynamodb.Keys;
-        return {
-            operation: 'deleteItem',
-            params: request
-        };
-    } else {
-        request.Item = record.dynamodb.NewImage;
-        return {
-            operation: 'putItem',
-            params: request
-        };
-    }
+  } else {
+    request.Item = record.dynamodb.NewImage;
+    return {
+      operation: 'putItem',
+      params: request
+    };
+  }
 }
 
 function buildTasksForEachRecord(records, dynamoClient) {
-    let requests = records.map(createReplicateRequest);
-    //console.log(requests);
-    let tasks = requests.map(function (request) {
-        return dynamoClient[request.operation](request.params).promise();
+  let requests = records.map(createReplicateRequest);
+  //console.log(requests);
+  let tasks = requests.map(function (request) {
+    return dynamoClient[request.operation](request.params).promise();
   });
 
   return tasks;
 }
 
-function replicateRecordsToAccount(records, accountNumber) {
-
-  console.log('Replicating records to %d AWS account', accountNumber);
-
-  function assumeRole() {
-    let stsClient = new AWS.STS();
-    let stsParams = {
-      RoleArn: getIAMRoleInTargetAccount(accountNumber),
-      RoleSessionName: '' + new Date().getTime()
-    };
-    return stsClient.assumeRole(stsParams).promise();
-  }
-
-  function createDynamoClientWithAssumedRole(stsResponse) {
-    let credentials = new AWS.Credentials(
-      stsResponse.Credentials.AccessKeyId,
-      stsResponse.Credentials.SecretAccessKey,
-      stsResponse.Credentials.SessionToken
-    );
-    return new AWS.DynamoDB({ credentials: credentials });
-  }
-
-  function writeEachRecord(dynamoClient) {
-    let tasks = buildTasksForEachRecord(records, dynamoClient);
-    return Promise.all(tasks);
-  }
-
-  return assumeRole()
-    .then(createDynamoClientWithAssumedRole)
-    .then(writeEachRecord);
-}
-
 exports.handler = function (event, context) {
+  const DESTINATION_ACCOUNTS = process.env.DESTINATION_ACCOUNTS.split(',').map(x => x.trim());
+  const WRITER_ROLE_NAME = process.env.WRITER_ROLE_NAME;
+
+  function getIAMRoleInTargetAccount(accountNumber) {
+    var arn = `arn:aws:iam::${accountNumber}:role/${WRITER_ROLE_NAME}`;
+    return arn;
+  }
+
+  function replicateRecordsToAccount(records, accountNumber) {
+
+    console.log('Replicating records to %d AWS account', accountNumber);
+
+    function assumeRole() {
+      let stsClient = new AWS.STS();
+      let stsParams = {
+        RoleArn: getIAMRoleInTargetAccount(accountNumber),
+        RoleSessionName: '' + new Date().getTime()
+      };
+      return stsClient.assumeRole(stsParams).promise();
+    }
+
+    function createDynamoClientWithAssumedRole(stsResponse) {
+      let credentials = new AWS.Credentials(
+        stsResponse.Credentials.AccessKeyId,
+        stsResponse.Credentials.SecretAccessKey,
+        stsResponse.Credentials.SessionToken
+      );
+      return new AWS.DynamoDB({ credentials: credentials });
+    }
+
+    function writeEachRecord(dynamoClient) {
+      let tasks = buildTasksForEachRecord(records, dynamoClient);
+      return Promise.all(tasks);
+    }
+
+    return assumeRole()
+      .then(createDynamoClientWithAssumedRole)
+      .then(writeEachRecord);
+  }
+
   let records = event.Records;
   console.log('Received %d records from dynamo streams', records.length);
 
@@ -99,6 +94,15 @@ exports.handler = function (event, context) {
 
     return Promise.all(tasks)
       .then(ts => ts.some(t => t instanceof Error) ? Promise.reject(new Error('Failed to write to one or more accounts.')) : Promise.resolve());
+  }
+
+  /* This function returns a promise of the configuration required
+   * to run this function. The current implementation just returns
+   * a constant but a DynamoDB or S3 lookup would be easy. */
+  function getConfiguration() {
+    return Promise.resolve({
+      DestinationAccounts: DESTINATION_ACCOUNTS
+    });
   }
 
   return getConfiguration(context).then(main).then(context.succeed, context.fail);
