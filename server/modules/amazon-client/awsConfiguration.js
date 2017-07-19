b39a3dd116a3548be0c91f151b4430e54213b8d4
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-07-19 14:16:40 +0100

    PD-327 module to get AWS client options from env or env type

---
+++ server/modules/amazon-client/awsConfiguration.js
@@ -0,0 +1,47 @@
+/**
+ * This module provides functions for retrieving data required
+ * to interact with managed environments in other accounts.
+ */
+
+'use strict';
+
+let Promise = require('bluebird');
+let accounts = require('modules/data-access/accounts');
+let environments = require('modules/data-access/configEnvironments');
+let environmentTypes = require('modules/data-access/configEnvironmentTypes');
+
+function getAwsOptionsForEnvironment(environmentName) {
+  return environments.get({ EnvironmentName: environmentName })
+    .then(environment => (environment !== null
+      ? getAwsOptionsForEnvironmentType(environment.Value.EnvironmentType)
+      : null));
+}
+
+function getAwsOptionsForEnvironmentType(environmentTypeName) {
+  if (typeof environmentTypeName !== 'string') {
+    return Promise.resolve(null);
+  }
+  let environmentTypeP = environmentTypes.get({ EnvironmentType: environmentTypeName });
+  let accountP = environmentTypeP.then(environmentType => (environmentType !== null
+    ? accounts.get({ AccountNumber: parseInt(environmentType.Value.AWSAccountNumber, 10) })
+    : null));
+  return Promise.join(accountP, environmentTypeP,
+    (account, environmentType) => {
+      if (account !== null && environmentType !== null) {
+        let { RoleArn: roleArn } = account;
+        let { Value: { Region: region } = {} } = environmentType;
+        return Object.assign(...[
+          {},
+          region !== undefined && region !== null ? { region } : {},
+          roleArn !== undefined && roleArn !== null ? { roleArn } : {}
+        ]);
+      } else {
+        return null;
+      }
+    });
+}
+
+module.exports = {
+  getAwsOptionsForEnvironment,
+  getAwsOptionsForEnvironmentType
+};
